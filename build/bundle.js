dda4dd984683c3ef2de7b1f173f6060358fedd05
Wolfgang Egger <w.egger@framlin.com>
2017-02-18 09:45:21 +0100

    - data for rfid 65466597

--- build/bundle.js
+++ build/bundle.js
@@ -22820,6 +22820,54 @@
 /***/ function(module, exports) {
 
 	"use strict";
+	
+	module.exports = [{
+	    name: "",
+	    description: [{
+	        type: "text",
+	        value: "NCP1117 5V - 1.0 A Low-Dropout Positive Fixed and Adjustable Voltage Regulators"
+	    }, {
+	        type: "link",
+	        target: "https://www.ehajo.de/bauelemente/aktiv/spannungsversorgung/ncp1117-5v.html?search=200.047&description=true",
+	        value: "eHaJo Product Page"
+	    }, {
+	        type: "link",
+	        target: "http://www.onsemi.com/pub_link/Collateral/NCP1117-D.PDF",
+	        value: "onsemi Datasheet (PDF)"
+	    }]
+	}, {
+	    name: "",
+	    description: [{
+	        type: "text",
+	        value: "50-mA, 24-V, 3.2-μA Supply Current Low-Dropout Linear Regulator in SC70 package"
+	    }, {
+	        type: "link",
+	        target: "https://www.ehajo.de/bauelemente/aktiv/spannungsversorgung/tps71533.html?search=100.024&description=true",
+	        value: "eHaJo Product Page"
+	    }, {
+	        type: "link",
+	        target: "http://www.ti.com/lit/ds/symlink/tps715.pdf",
+	        value: "TI Datasheet (PDF)"
+	    }]
+	}, {
+	    name: "",
+	    description: [{
+	        type: "text",
+	        value: "WS2812B - Intelligent control LED integrated light source"
+	    }, {
+	        type: "link",
+	        target: "https://www.ehajo.de/bauelemente/aktiv/optoelektronik/ws2812b-intelligente-rgb-led.html?search=200.054&description=true",
+	        value: "eHaJo Product Page"
+	    }, {
+	        type: "link",
+	        target: "https://cdn-shop.adafruit.com/datasheets/WS2812B.pdf",
+	        value: "Worldsemi Datasheet (PDF)"
+	    }]
+	}];
+	
+	/**
+	 65466597
+	 */
 
 /***/ },
 /* 216 */
